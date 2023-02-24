4df4a55aa129550b343519ec2309f4de694b4b58
vinz <123943529+vcazu@users.noreply.github.com>
2023-02-24 23:01:40 +0800

    Add logic to return a string a n of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+let greeting = "hey";
+const repeatString = function (string, num) {
+  for (let i = 0; i < num; i++) {
+    greeting += string;
+    return greeting;
+  }
 };
+
+repeatString(greeting, 10);
 
 // Do not edit below this line
 module.exports = repeatString;
