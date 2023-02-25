3067e5aec959a154189390771f66df00f73515a0
vinz <123943529+vcazu@users.noreply.github.com>
2023-02-25 21:31:09 +0800

    Add logic to removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  return array.filter((arg) => !args.includes(arg));
 };
 
 // Do not edit below this line
