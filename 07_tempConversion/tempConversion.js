a5d58c7c4ed967d7db5aebf471882d59bebb1812
vinz <123943529+vcazu@users.noreply.github.com>
2023-02-25 23:35:25 +0800

    Add logic to tempConversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let temp = (fahrenheit - 32) * (5 / 9);
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let temp = celsius * (9 / 5) + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
