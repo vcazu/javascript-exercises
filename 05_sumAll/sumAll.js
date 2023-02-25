5da4013d01375a6012d5b690798e42c031f7652e
vinz <123943529+vcazu@users.noreply.github.com>
2023-02-25 22:36:36 +0800

    Add logic to sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (operandL, operandR) {
+  let sum = 0;
 
+  switch (true) {
+    case typeof operandL !== "number":
+    case typeof operandR !== "number":
+    case operandL < 0:
+    case operandR < 0:
+      return "ERROR";
+    case operandL < operandR:
+      for (; operandL <= operandR; operandL++) {
+        sum += operandL;
+      }
+      break;
+    case operandL > operandR:
+      for (; operandL >= operandR; operandL--) {
+        sum += operandL;
+      }
+      break;
+    default:
+      return "something went wrong";
+  }
+  return sum;
 };
 
 // Do not edit below this line
