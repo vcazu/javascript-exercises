ca13a0ddc14f97f50d4340782eec492989208a24
vinz <123943529+vcazu@users.noreply.github.com>
2023-03-16 05:21:36 +0800

    Update palindrome func

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (param) {
+  let punctuation = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  let noPunctuation = param
+    .split("")
+    .filter((char) => punctuation.indexOf(char) === -1)
+    .join("")
+    .toLowerCase();
 
+  let reversed = noPunctuation.split("").reverse().join("").toLowerCase();
+
+  if (noPunctuation === reversed) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
