e9ccf4b665b56f9fa2bb66309bd40327771327e3
vinz <123943529+vcazu@users.noreply.github.com>
2023-03-17 06:50:18 +0800

    Update fibonacci func

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (arg) {
+  if (+arg < 1) {
+    return "OOPS";
+  }
 
+  let arr = [0, 1];
+
+  for (let i = arr.length; i <= +arg; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+
+  return arr[arr.length - 1];
 };
 
+//https://www.cuemath.com/algebra/fibonacci-numbers/
+//https://byjus.com/maths/fibonacci-numbers/
 // Do not edit below this line
 module.exports = fibonacci;
