7ceeb45a9063bfd7e22b09fc5ed32a74b015b264
vinz <123943529+vcazu@users.noreply.github.com>
2023-03-18 04:37:51 +0800

    Update findTheOldest func

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, youngest) => {
+    if (!oldest.yearOfDeath) {
+      oldest.yearOfDeath = new Date().getFullYear();
+    } else if (!youngest.yearOfDeath) {
+      youngest.yearOfDeath = new Date().getFullYear();
+    }
 
+    let old = oldest.yearOfDeath - oldest.yearOfBirth;
+    let young = youngest.yearOfDeath - youngest.yearOfBirth;
+
+    return old < young ? youngest : oldest;
+  });
 };
 
 // Do not edit below this line
